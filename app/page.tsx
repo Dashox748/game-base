ac2948d3c2b0fbaaf1a3979a8498f4d63d1ebf0a
Hubert <hubert.grabke@gmail.com>
2023-02-07 20:03:24 +0100

    feat: add explore section under main banner with games and news

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,36 @@ export default async function Home() {
                     </Link>
                 </div>
             </div>
-
+            <div className={classes.homepage__main_text}>
+                {/*<span> All About </span>*/}
+                {/*<span style={{ color: "#5163e6" }}> Games </span>*/}
+                {/*<span> In One </span>*/}
+                {/*<span style={{ color: "#ff67d1" }}> Spot </span>*/}
+            </div>
+            <div
+                style={{ display: "flex", gap: "3rem", width: "80%", margin: "0 auto" }}
+            >
+                <div style={{ width: "60%", position: "relative", height: "400px" }}>
+                    <Image src={"/games_banner.webp"} alt={"games"} fill />
+                    <Link
+                        href={"/games/section1/popular"}
+                        className={classes.homepage__button_games}
+                    >
+                        Games
+                    </Link>
+                </div>
+                <div style={{ width: "40%", position: "relative", height: "400px" }}>
+                    <Image
+                        src={"/news_banner.jpg"}
+                        alt={"games"}
+                        fill
+                        style={{ objectFit: "cover" }}
+                    />
+                    <Link href={"/news"} className={classes.homepage__button_news}>
+                        News
+                    </Link>
+                </div>
+            </div>
         </main>
     );
 }
