65c72dd6e5c9e987a1759bd6e6053b1d8b6a1a4a
Hubert <hubert.grabke@gmail.com>
2023-02-07 19:59:12 +0100

    feat: add main banner with hotest news to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
-// access_token: 'yrdniavh81hqy3s53eyb0h5riuiwis',
-//     expires_in: 5104167,
-//     token_type: 'bearer'
-
-import MostPopular from "@/app/games/page";
+import Image from "next/image";
+import classes from "./page.module.css";
+import Link from "next/link";
 
 export default async function Home() {
-  return (
-    <main>
-      {/* @ts-ignore */}
-      <h1>Bedzie jakiś ładny homepage</h1>
-    </main>
-  );
+    return (
+        <main>
+            <div className={classes.banner__image_holder}>
+                <Image
+                    src="/hogwarts_legacy2.jpg"
+                    fill
+                    alt="game banner"
+                    style={{ objectFit: "cover", opacity: "70%" }}
+                />
+                <div className={classes.banner__image_title}>
+                    <span>Hogwarts Legacy </span>
+                    <span>is finnaly Here</span>
+                    <Link className={classes.check_button} href={"/game/906547"}>
+                        Check it Out
+                    </Link>
+                </div>
+            </div>
+
+        </main>
+    );
 }
